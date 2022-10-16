0e75a5a71e6344abaec1f45c82e59d89fefcd4e0
jacky32 <patrki@atlas.cz>
2022-10-16 13:37:50 +0200

    Add click logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import "./App.css";
-import Board from "./components/Board";
 import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import "./styles/Board.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [currentCount, setCurrentCount] = useState(0);
+  const [bestCount, setBestCount] = useState(0);
+  const [childData, setChildData] = useState(false);
+  const [clickedChildren, setClickedChildren] = useState([]);
 
-  // useEffect(() => {
-  //   const addOne = () => setCount(count + 1);
+  const handleClick = (e) => {
+    const newChild = e.target;
+    if (clickedChildren.includes(newChild)) {
+      setClickedChildren([]);
+      setCurrentCount(0);
+    } else {
+      setClickedChildren([...clickedChildren, newChild]);
+      setCurrentCount(currentCount + 1);
+    }
+  };
 
-  //   document.addEventListener("click", addOne);
-  // }, [count]);
+  useEffect(() => {
+    handleBestCounter();
+    // return () => {
+    // };
+  }, [currentCount]);
+
+  useEffect(() => {
+    if (childData === true) {
+    }
+  }, [childData]);
+
+  const handleBestCounter = () => {
+    if (currentCount >= bestCount) setBestCount(currentCount);
+  };
+
+  const resetCounter = () => {};
+
+  const numbers = [1, 4, 5, 6, 7, 8, 9, 10, 11, 15, 17, 19];
 
   return (
     <div className="App">
       <header className="App-header">
         <div>aa</div>
-        {/* <div>You clicked {count} times</div> */}
+        <div>You clicked {currentCount} times</div>
+        <div>Best: {bestCount} times</div>
       </header>
       <div className="App-board">
-        <Board />
+        <div className="Board">
+          {numbers.map((number) => {
+            return (
+              <Card
+                key={number}
+                number={number}
+                resetClicked={setChildData}
+                handleClick={handleClick}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,6 @@ import "../styles/Card.css";
 import { useEffect, useState } from "react";
 
 const Card = (props) => {
-  const [clicks, setClicks] = useState(0);
-
-  // useEffect(() => {
-  //   const addOne = () => setClicks(clicks + 1);
-
-  //   document.addEventListener("click", addOne);
-  // }, [clicks]);
-
-  const handleClick = (e) => {
-    setClicks(clicks + 1);
-  };
-
   const image = (i) => {
     if (i <= 9)
       return `https://art.hearthstonejson.com/v1/render/latest/enUS/256x/EX1_00${i}.png`;
@@ -22,16 +10,14 @@ const Card = (props) => {
 
   const imgDom = () => {
     return (
-      <div className="Card-crop" onClick={(e) => handleClick(e)}>
+      <div className="Card-crop">
         <img
           src={image(props.number)}
           className="Card"
           draggable="false"
-          alt={"Image number" + props.number}
+          alt={"Image number " + props.number}
+          onClick={(e) => props.handleClick(e)}
         />
-        <div>
-          <div>You clicked {clicks} times</div>
-        </div>
       </div>
     );
   };
